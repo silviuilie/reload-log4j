a6940d4d57e9b4ed073b444dcde03d49429affe6
silviuilie <silviu.ilie@gmail.com>
2014-09-03 22:58:13 +0200

    update test

--- src/test/java/eu/pm/tools/log4j/servlet/Log4jUtilityServletTest.java
+++ src/test/java/eu/pm/tools/log4j/servlet/Log4jUtilityServletTest.java
@@ -1,15 +1,27 @@
 package eu.pm.tools.log4j.servlet;
 
-import eu.pm.tools.log4j.servlet.Log4jUtilityServlet;
+import eu.pm.tools.log4j.Log4jUtility;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
 
 /**
  * Created by silviu
  * Date: 01/03/14 / 22:21562
  * <p/>
  * <p>
- * TODO :comment me!
+ * {@link Log4jUtilityServlet} test.
  * </p>
  * <p/>
  * <br/>
@@ -19,16 +31,68 @@ import org.junit.Test;
  */
 public class Log4jUtilityServletTest {
 
-    Log4jUtilityServlet tested = new Log4jUtilityServlet();
+    Log4jUtilityServlet servlet = new Log4jUtilityServlet();
+
+    Log4jUtility mockLog4jUtility = mock(Log4jUtility.class);
+    ServletConfig mockServletConfig = mock(ServletConfig.class);
+    ServletContext ctx = mock(ServletContext.class);
+
+    HttpSession mockSession = mock(HttpSession.class);
+    HttpServletRequest reqMock = mock(HttpServletRequest.class);
+    HttpServletResponse respMock = mock(HttpServletResponse.class);
 
 
     @Before
-    public void init() {
-        tested.set
+    public void init() throws ServletException {
+        reset(mockLog4jUtility);
+        reset(mockServletConfig);
+        reset(ctx);
+        reset(mockSession);
+        reset(reqMock);
+        reset(respMock);
+
+        when(mockServletConfig.getServletContext()).thenReturn(ctx);
+
+        when(ctx.getInitParameter(Log4jUtility.LOG4J_UTILITY_PACKAGE_ATTR_NAME))
+                .thenReturn(Log4jUtility.LOG4J_UTILITY_PACKAGE_ATTR_NAME + ".test");
+
+        when(ctx.getInitParameter(Log4jUtility.LOG4J_UTILITY_HOME_URL))
+                .thenReturn(Log4jUtility.LOG4J_UTILITY_HOME_URL + ".test");
+        when(ctx.getInitParameter(Log4jUtility.LOG4J_UTILITY_JSP_LOCATION_ATTR_NAME))
+                .thenReturn(Log4jUtility.LOG4J_UTILITY_JSP_LOCATION_ATTR_NAME + ".test");
+        when(ctx.getInitParameter(Log4jUtility.LOG4J_UTILITY_AUTH_ATTR_NAME))
+                .thenReturn(Log4jUtility.LOG4J_UTILITY_AUTH_ATTR_NAME + ".test");
+
+        when(reqMock.getSession()).thenReturn(mockSession);
+        when(reqMock.getRequestURI()).thenReturn(Log4jUtility.LOG4J_UTILITY_HOME_URL);
+        when(mockSession.getServletContext()).thenReturn(ctx);
+
+        servlet.setLog4jUtility(mockLog4jUtility);
+        servlet.init(mockServletConfig);
     }
 
     @Test
-    public void test() {
+    public void doGetNotAuthorized() throws ServletException, IOException {
 
+        servlet.doGet(reqMock, respMock);
+
+        verify(mockLog4jUtility, times(1)).isAuthorized(mockSession);
+        verify(respMock, times(1)).sendRedirect(null);
+    }
+
+    @Test
+    public void doGetAuthorized() throws ServletException, IOException {
+        RequestDispatcher requestDispatcherMock = mock(RequestDispatcher.class);
+
+        when(ctx.getRequestDispatcher(Log4jUtility.LOG4J_UTILITY_JSP_LOCATION_ATTR_NAME + ".test"
+                + "log4jlevelReload.jsp")).thenReturn(requestDispatcherMock);
+
+        when(reqMock.getSession()).thenReturn(mockSession);
+        when(reqMock.getRequestURI()).thenReturn(Log4jUtility.LOG4J_UTILITY_HOME_URL);
+        when(mockLog4jUtility.isAuthorized(mockSession)).thenReturn(true);
+
+        servlet.doGet(reqMock, respMock);
+
+        verify(requestDispatcherMock, times(1)).forward(reqMock, respMock);
     }
 }
