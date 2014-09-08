909a23eb1bc27c82b8974865e49c9f4d9ebd14d0
silviuilie <silviu.ilie@gmail.com>
2014-09-08 22:50:59 +0200

    test update

--- src/test/java/eu/pm/tools/log4j/servlet/Log4jUtilityServletTest.java
+++ src/test/java/eu/pm/tools/log4j/servlet/Log4jUtilityServletTest.java
@@ -1,8 +1,12 @@
 package eu.pm.tools.log4j.servlet;
 
+import eu.pm.tools.log4j.Log4jApplicationContext;
 import eu.pm.tools.log4j.Log4jUtility;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -13,7 +17,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import static eu.pm.tools.log4j.Log4jUtility.LOG4J_UTILITY_FIND_CLASS_URL;
+import static eu.pm.tools.log4j.Log4jUtility.LOG4J_UTILITY_RELOAD_URL;
 import static org.mockito.Mockito.*;
 
 /**
@@ -42,6 +49,18 @@ public class Log4jUtilityServletTest {
     HttpServletResponse respMock = mock(HttpServletResponse.class);
 
 
+    public class ClassToFindByName {
+
+    }
+
+    Log4jApplicationContext.ReloadAuthorization mockAuth = new Log4jApplicationContext.ReloadAuthorization() {
+        @Override
+        public boolean authorize(HttpSession session) {
+            return true;
+        }
+    };
+
+
     @Before
     public void init() throws ServletException {
         reset(mockLog4jUtility);
@@ -54,14 +73,18 @@ public class Log4jUtilityServletTest {
         when(mockServletConfig.getServletContext()).thenReturn(ctx);
 
         when(ctx.getInitParameter(Log4jUtility.LOG4J_UTILITY_PACKAGE_ATTR_NAME))
-                .thenReturn(Log4jUtility.LOG4J_UTILITY_PACKAGE_ATTR_NAME + ".test");
+                .thenReturn("eu");
 
         when(ctx.getInitParameter(Log4jUtility.LOG4J_UTILITY_HOME_URL))
                 .thenReturn(Log4jUtility.LOG4J_UTILITY_HOME_URL + ".test");
+
         when(ctx.getInitParameter(Log4jUtility.LOG4J_UTILITY_JSP_LOCATION_ATTR_NAME))
                 .thenReturn(Log4jUtility.LOG4J_UTILITY_JSP_LOCATION_ATTR_NAME + ".test");
+
+/*
         when(ctx.getInitParameter(Log4jUtility.LOG4J_UTILITY_AUTH_ATTR_NAME))
-                .thenReturn(Log4jUtility.LOG4J_UTILITY_AUTH_ATTR_NAME + ".test");
+                .thenReturn(mockAuth.getClass().getSimpleName());
+*/
 
         when(reqMock.getSession()).thenReturn(mockSession);
         when(reqMock.getRequestURI()).thenReturn(Log4jUtility.LOG4J_UTILITY_HOME_URL);
@@ -70,6 +93,79 @@ public class Log4jUtilityServletTest {
         servlet.setLog4jUtility(mockLog4jUtility);
         servlet.init(mockServletConfig);
     }
+
+    @After
+    public void validate() {
+//        validateMockitoUsage();
+    }
+
+    @Test
+    public void doPostEmpty() throws ServletException, IOException {
+        servlet.doPost(reqMock, respMock);
+    }
+
+
+    @Test
+    public void doPostFindClass() throws ServletException, IOException {
+        String classNameFragment = "ToFindBy";
+        PrintWriter mockWriter = mock(PrintWriter.class);
+
+        when(reqMock.getSession()).thenReturn(mockSession);
+        when(reqMock.getParameter("name")).thenReturn(classNameFragment);
+        when(reqMock.getRequestURI()).thenReturn(LOG4J_UTILITY_FIND_CLASS_URL);
+        when(mockLog4jUtility.isAuthorized(mockSession)).thenReturn(true);
+        when(mockLog4jUtility.log4jFindClass(classNameFragment, mockSession)).thenReturn(classNameFragment);
+        when(respMock.getWriter()).thenReturn(mockWriter);
+
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                return null;
+            }
+        }).when(mockWriter).write(classNameFragment);
+
+        servlet.doPost(reqMock, respMock);
+
+        verify(mockWriter, times(1)).write(classNameFragment);
+        verify(reqMock, times(2)).getSession();
+        verify(reqMock, times(1)).getParameter("name");
+        verify(reqMock, times(1)).getRequestURI();
+        verify(mockLog4jUtility, times(1)).isAuthorized(mockSession);
+        verify(respMock, times(1)).getWriter();
+    }
+
+    @Test
+    public void doPostReloadClassPriority() throws ServletException, IOException {
+        String className = "ToFindBy";
+        String priority = "DEBUG";
+        PrintWriter mockWriter = mock(PrintWriter.class);
+
+        when(reqMock.getSession()).thenReturn(mockSession);
+        when(reqMock.getParameter("target")).thenReturn(className);
+        when(reqMock.getParameter("priority")).thenReturn(priority);
+        when(reqMock.getRequestURI()).thenReturn(LOG4J_UTILITY_RELOAD_URL);
+        when(mockLog4jUtility.isAuthorized(mockSession)).thenReturn(true);
+        when(mockLog4jUtility.setPriority(className,priority, mockSession)).thenReturn("");
+        when(respMock.getWriter()).thenReturn(mockWriter);
+
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                return null;
+            }
+        }).when(mockWriter).write("");
+
+        servlet.doPost(reqMock, respMock);
+
+        verify(mockWriter, times(1)).write("");
+        verify(reqMock, times(2)).getSession();
+        verify(reqMock, times(1)).getParameter("target");
+        verify(reqMock, times(1)).getParameter("priority");
+        verify(reqMock, times(1)).getRequestURI();
+        verify(mockLog4jUtility, times(1)).isAuthorized(mockSession);
+        verify(respMock, times(1)).getWriter();
+    }
+
 
     @Test
     public void doGetNotAuthorized() throws ServletException, IOException {
